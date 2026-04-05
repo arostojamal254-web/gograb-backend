bb80627a1f252143b02fb948ac42855f1a6865a7
Osman Jamal <you@example.com>
2026-04-05 20:13:04 +0300

    Disable Redis to prevent crash

--- redis.js
+++ redis.js
@@ -1,4 +1,1 @@
-const redis = require('redis');
-const client = redis.createClient();
-client.connect().then(() => console.log('Redis connected'));
-module.exports = client;
+"module.exports = { get: () => null, set: () => null, hset: () => null };" 
